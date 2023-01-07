74ad72f40967149ebdb385d3d454f19f719f3956
Yariv <FroznKnight@gmail.com>
2023-01-08 00:53:28 +0200

    Made ui and logic fixes

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-function getPlayerChoice() {
-  let playerChoice = prompt("Rock, Paper or Scissors?");
-  return playerChoice.toLowerCase();
-}
+const roundResult = document.querySelector("#roundResult");
+const startGame = document.querySelector("#start");
+const restartGame = document.querySelector("#restart");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const playerCount = document.querySelector("#playerScore");
+const computerCount = document.querySelector("#pcScore");
+
+let playerScore = 0;
+let computerScore = 0;
+let sameResult = 0;
+let gameOver = true;
 
 function getComputerChoice() {
   let random = Math.floor(Math.random() * 3) + 1;
@@ -12,92 +21,196 @@ function getComputerChoice() {
   } else return "scissors";
 }
 
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissors = document.querySelector("#scissors");
+rock.disabled = true;
+rock.classList.add("disabled");
+paper.disabled = true;
+paper.classList.add("disabled");
+scissors.disabled = true;
+scissors.classList.add("disabled");
+restartGame.disabled = true;
+restartGame.classList.add("disabled");
 
-rock.addEventListener("click", () => {
-  let cpChoice = getComputerChoice();
-  if (cpChoice === "rock") {
-    console.log("Draw");
-  } else if (cpChoice === "scissors") {
-    console.log("Win");
-  } else {
-    console.log("Lose");
+startGame.addEventListener("click", () => {
+  gameOver = false;
+  playerCount.textContent = `${playerScore}`;
+  computerCount.textContent = `${computerScore}`;
+  roundResult.textContent = "Best of 5";
+  rock.disabled = false;
+  rock.classList.remove("disabled");
+  paper.disabled = false;
+  paper.classList.remove("disabled");
+  scissors.disabled = false;
+  scissors.classList.remove("disabled");
+  restartGame.disabled = false;
+  restartGame.classList.remove("disabled");
+  startGame.classList.add("disabled");
+  startGame.disabled = true;
+  if (!gameOver) {
+    rock.addEventListener("click", () => {
+      let cpChoice = getComputerChoice();
+      if (cpChoice === "rock") {
+        sameResult = 0;
+        sameResult++;
+        console.log("Draw");
+        if (sameResult > 1) {
+          roundResult.textContent = `You both went with the rock, a draw it is. ...(${sameResult})`;
+        } else {
+          roundResult.textContent =
+            "You both went with the rock, a draw it is.";
+        }
+      } else if (cpChoice === "scissors") {
+        sameResult = 0;
+        sameResult++;
+        console.log("Win");
+        playerScore++;
+        if (sameResult > 1) {
+          roundResult.textContent = `Rock smashes scissors to shards! You win! ...(${sameResult})`;
+          playerCount.textContent = `${playerScore}`;
+        } else {
+          roundResult.textContent = `Rock smashes scissors to shards! You win!`;
+          playerCount.textContent = `${playerScore}`;
+        }
+      } else {
+        sameResult = 0;
+        sameResult++;
+        console.log("Lose");
+        computerScore++;
+        computerCount.textContent = `${computerScore}`;
+        if (sameResult > 1)
+          roundResult.textContent = `Your rock gets wrapped in a deadly paper, and suffocates to death, you lose. ...(${sameResult})`;
+        else {
+          roundResult.textContent = `Your rock gets wrapped in a deadly paper, and suffocates to death, you lose.`;
+        }
+      }
+      if (playerScore == 5) {
+        gameOver = true;
+        roundResult.textContent = "Game over, you win!";
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+        rock.classList.add("disabled");
+        paper.classList.add("disabled");
+        scissors.classList.add("disabled");
+        pcScore.style.color = "red";
+        playerCount.style.color = "green";
+      } else if (computerScore == 5) {
+        gameOver = true;
+        roundResult.textContent = "Game over, you lose.";
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+        rock.classList.add("disabled");
+        paper.classList.add("disabled");
+        scissors.classList.add("disabled");
+        pcScore.style.color = "green";
+        playerCount.style.color = "red";
+      }
+    });
+
+    paper.addEventListener("click", () => {
+      let cpChoice = getComputerChoice();
+      if (cpChoice === "paper") {
+        console.log("Draw");
+        roundResult.textContent = `If this was an airplane fight someone might've won, but it is not, a draw it is.`;
+      } else if (cpChoice === "rock") {
+        console.log("Win");
+        playerScore++;
+        roundResult.textContent =
+          "You hug the opponents rock to death with your affection and love, you win!";
+        playerCount.textContent = `${playerScore}`;
+      } else {
+        console.log("Lose");
+        computerScore++;
+        roundResult.textContent =
+          "You got a makeover by the opponents scissors... and not a pretty one at that, you lose.";
+        computerCount.textContent = `${computerScore}`;
+      }
+      if (playerScore == 5) {
+        gameOver = true;
+        roundResult.textContent = "Game over, you win!";
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+        rock.classList.add("disabled");
+        paper.classList.add("disabled");
+        scissors.classList.add("disabled");
+        pcScore.style.color = "red";
+        playerCount.style.color = "green";
+      } else if (computerScore == 5) {
+        gameOver = true;
+        roundResult.textContent = "Game over, you lose.";
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+        rock.classList.add("disabled");
+        paper.classList.add("disabled");
+        scissors.classList.add("disabled");
+        pcScore.style.color = "green";
+        playerCount.style.color = "red";
+      }
+    });
+
+    scissors.addEventListener("click", () => {
+      let cpChoice = getComputerChoice();
+      if (cpChoice === "scissors") {
+        console.log("Draw");
+        roundResult.textContent = `Would have been cooler if this was a sword fight, but a scissor fight... meh, it's a draw.`;
+      } else if (cpChoice === "paper") {
+        console.log("Win");
+        playerScore++;
+        roundResult.textContent =
+          "Snip snip! as your opponents paper shivers with fear not knowing the grim fate that awaits it, you win!";
+        playerCount.textContent = `${playerScore}`;
+      } else {
+        console.log("Lose");
+        computerScore++;
+        roundResult.textContent =
+          "You stare at the metallic blades reflection of what used to be a perfectly cutting scissors once... , you lose.";
+        computerCount.textContent = `${computerScore}`;
+      }
+      if (playerScore == 5) {
+        gameOver = true;
+        roundResult.textContent = "Game over, you win!";
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+        rock.classList.add("disabled");
+        paper.classList.add("disabled");
+        scissors.classList.add("disabled");
+        pcScore.style.color = "red";
+        playerCount.style.color = "green";
+      } else if (computerScore == 5) {
+        gameOver = true;
+        roundResult.textContent = "Game over, you lose.";
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+        rock.classList.add("disabled");
+        paper.classList.add("disabled");
+        scissors.classList.add("disabled");
+        pcScore.style.color = "green";
+        playerCount.style.color = "red";
+      }
+    });
   }
 });
 
-paper.addEventListener("click", () => {
-  let cpChoice = getComputerChoice();
-  if (cpChoice === "paper") {
-    console.log("Draw");
-  } else if (cpChoice === "rock") {
-    console.log("Win");
-  } else {
-    console.log("Lose");
-  }
+restartGame.addEventListener("click", () => {
+  playerScore = 0;
+  computerScore = 0;
+  pcScore.style.color = "black";
+  playerCount.style.color = "black";
+  gameOver = false;
+  playerCount.textContent = `${playerScore}`;
+  computerCount.textContent = `${computerScore}`;
+  roundResult.textContent = "Best of 5";
+  rock.disabled = false;
+  rock.classList.remove("disabled");
+  paper.disabled = false;
+  paper.classList.remove("disabled");
+  scissors.disabled = false;
+  scissors.classList.remove("disabled");
+  startGame.classList.add("disabled");
+  startGame.disabled = true;
+  console.clear();
 });
-
-scissors.addEventListener("click", () => {
-  let cpChoice = getComputerChoice();
-  if (cpChoice === "scissors") {
-    console.log("Draw");
-  } else if (cpChoice === "paper") {
-    console.log("Win");
-  } else {
-    console.log("Lose");
-  }
-});
-
-let playerCount = 0;
-let computerCount = 0;
-
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
-    console.log(
-      `It's a tie between ${playerSelection} and ${computerSelection}!`
-    );
-  } else {
-    if (playerSelection === "rock" && computerSelection === "paper") {
-      console.log("Rock loses to Paper, you lose.");
-      computerCount++;
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-      console.log("Rock beats Scissors, you win.");
-      playerCount++;
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-      console.log("Paper beats Rock, you win.");
-      playerCount++;
-    } else if (
-      playerSelection === "paper" &&
-      computerSelection === "scissors"
-    ) {
-      console.log("Paper loses to Scissors, you lose.");
-      computerCount++;
-    } else if (playerSelection === "scissors" && computerSelection === "Rock") {
-      console.log("Scissors lose to Rock, you lose.");
-      computerCount++;
-    } else if (
-      playerSelection === "scissors" &&
-      computerSelection === "paper"
-    ) {
-      console.log("Scissors beats Paper, you win.");
-      playerCount++;
-    }
-  }
-}
-
-function game() {
-  for (let i = 0; i < 5; i++) {
-    playRound(getPlayerChoice(), getComputerChoice());
-    console.log(`You:${playerCount} | Computer:${computerCount}`);
-  }
-  if (playerCount > computerCount) {
-    console.log("Congratulations! you beat the computer.");
-  } else if (playerCount == computerCount) {
-    console.log("Well what do you know, it's a tie.");
-  } else {
-    console.log("You lost to the computer.");
-  }
-  playerCount = 0;
-  computerCount = 0;
-}
